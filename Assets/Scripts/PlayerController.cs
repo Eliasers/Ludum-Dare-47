13ab43ab8eb51f60190d52bf8a9396ab9b366bc5
Elias Ersson <32256443+Eliasers@users.noreply.github.com>
2020-10-03 13:02:26 +0200

    Moved some stuff around

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,16 +7,25 @@ public class PlayerController : MonoBehaviour
     public float movementSpeed;
     
     Rigidbody2D rb;
+    Animator anim;
+    SpriteMask sm;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rb.velocity.y);
+        float xMove = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(xMove * movementSpeed, rb.velocity.y);
+
+        
+
+        anim.SetFloat("Abs X Move", Mathf.Abs(xMove));
     }
 }
