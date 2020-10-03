3eaf655b1c0629d69d94b5e83137aa8d8a72bb67
Elias Ersson <32256443+Eliasers@users.noreply.github.com>
2020-10-03 19:31:03 +0200

    Began work on attacking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,15 @@ public class PlayerController : MonoBehaviour
 
     bool grounded;
 
+    bool isAttacking;
+    bool hasAttacked;
+    float timeAttacking;
+
+    float attackResolutionTime = 0.2f;
+    float attackRecoveryTime = 0.36f;
+    Vector2 attackOffset = new Vector2(0.7f, 0.7f);
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,20 +31,55 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        grounded = Physics2D.OverlapCircle(transform.position, 0.25f, StaticStuff.solidLayers) != null;
+        grounded = Physics2D.OverlapCircle(transform.position, 0.25f, StaticStuff.SolidLayers) != null;
         anim.SetBool("Grounded", grounded);
 
-        float xMove = Input.GetAxis("Horizontal");
-        anim.SetFloat("Abs X Move", Mathf.Abs(xMove));
-
-        rb.velocity = new Vector2(xMove * movementSpeed, rb.velocity.y);
-
-        if (xMove != 0) { transform.localScale = new Vector2((xMove > 0) ? 1 : -1, 1); }
-
-
-        if (Input.GetButtonDown("Jump") && grounded)
+        if (!isAttacking)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            float xMove = Input.GetAxis("Horizontal");
+            anim.SetFloat("Abs X Move", Mathf.Abs(xMove));
+
+            rb.velocity = new Vector2(xMove * movementSpeed, rb.velocity.y);
+
+            if (xMove != 0) { transform.localScale = new Vector2((xMove > 0) ? 1 : -1, 1); }
+
+
+            if (Input.GetButtonDown("Jump") && grounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            }
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                isAttacking = true;
+            }
+        } else {
+            //Attack logic
+            timeAttacking += Time.deltaTime;
+
+            if (timeAttacking >= attackResolutionTime)
+            {
+                if (!hasAttacked)
+                {
+                    Collider2D[] r = new Collider2D[5];
+                    ContactFilter2D cf = new ContactFilter2D();
+                    cf.layerMask = StaticStuff.Hurtables;
+                    cf.useLayerMask = true;
+                    Physics2D.OverlapCircle(transform.position, 0.1f, cf, r);
+                    for (int i = 0; i < r.Length; i++)
+                    {
+                        if (r[i] != null && r[i].gameObject != gameObject)
+                        {
+                            Debug.Log(r[i].gameObject.name);
+                            r[i].GetComponent<NPCController>().TakeDamage(gameObject, 1);
+                        }
+                    }
+
+
+                    hasAttacked = true;
+                }
+            }
+
         }
     }
 }
