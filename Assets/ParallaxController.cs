12303f6df00a03ae373f097bf6cb225fc3a27947
Elias Ersson <32256443+Eliasers@users.noreply.github.com>
2020-10-03 20:12:45 +0200

    Added parallax and worked on attacks

---
+++ Assets/ParallaxController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+    public float distance;
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = -player.position * distance/1000;
+        transform.localPosition = new Vector3(pos.x, pos.y, 10);
+    }
+}
