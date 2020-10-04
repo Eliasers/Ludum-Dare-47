39a5f4251d8bf8221b1f0490a1b6508e0b96504c
Elias Ersson <32256443+Eliaznizzle@users.noreply.github.com>
2020-10-04 22:04:39 +0200

    Added watchmaker

---
+++ Assets/WatchmakerController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatchmakerController : NPCController
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+}
